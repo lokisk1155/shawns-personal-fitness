785950216c9fc914680dcb171321b315f66dbde8
Lokisk1155 <shawn12345d@gmail.com>
2023-04-18 14:29:24 -0400

    functional map

--- src/pages/find-us.tsx
+++ src/pages/find-us.tsx
@@ -1,16 +1,16 @@
 import * as React from "react";
 import type { HeadFC, PageProps } from "gatsby";
 import Navigation from "../components/Navigation/indexNav";
-import { useLoadScript } from "@react-google-maps/api";
+import KatonahMap from "../components/Maps/KatonahMap";
+import { mainContainer } from "../styles/FindUs";
 
 const FindUsPage: React.FC<PageProps> = () => {
-  const { isLoaded, loadError } = useLoadScript({
-    googleMapsApiKey: `${process.env.GOOGLE_MAPS_API_KEY}`,
-  });
-
   return (
     <>
       <Navigation />
+      <main style={mainContainer}>
+        <KatonahMap />
+      </main>
     </>
   );
 };
---
+++ src/styles/FindUs.tsx
@@ -0,0 +1,7 @@
+import { CSSProperties } from "react";
+
+export const mainContainer: CSSProperties = {
+  marginTop: "130px",
+  width: "100vw",
+  height: "80vh",
+};
