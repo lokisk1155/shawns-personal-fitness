612602cf36a6df574a6fb08be865cbbed989244f
Lokisk1155 <shawn12345d@gmail.com>
2023-04-19 23:36:37 -0400

    png/jpeg for fb/insta incorportared into navlinks

--- src/components/Navigation/NavLinks.tsx
+++ src/components/Navigation/NavLinks.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { Link } from "gatsby";
 import { MainLinks } from "../../constants/mainLinks";
 import {
   mainLinksHeaderContainer,
@@ -7,15 +8,66 @@ import {
   celebrationText,
   NavigationContainer,
 } from "../../styles/Header";
-import { Link } from "gatsby";
+import facebook from "../../assets/facebook.png";
+import instagram from "../../assets/instagram.jpg";
 
 const NavLinks: React.FC = () => {
   return (
     <div style={NavigationContainer}>
       <header style={celebrationBannerHeader}>
         <h1 style={celebrationText}>
-          Celebrating 25 years of Shawn's Personal Fitness
+          Celebrating 29 years of Shawn's Personal Fitness
         </h1>
+        <address
+          style={{
+            position: "absolute",
+            right: "0",
+            marginRight: "10px",
+            width: "80px",
+            backgroundColor: "whitesmoke",
+            height: "65px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <a
+            href="https://www.facebook.com/shawnsfitness"
+            target="_blank"
+            rel="noopener"
+          >
+            <img
+              src={facebook}
+              style={{
+                height: "35px",
+                width: "35px",
+                backgroundColor: "transparent",
+                borderRadius: "10px",
+                margin: "auto",
+                padding: "1px",
+              }}
+              alt="Follow us on Facebook"
+            />
+          </a>
+          <a
+            href="https://www.instagram.com/shawnspersonalfitness/"
+            target="_blank"
+            rel="noopener"
+          >
+            <img
+              src={instagram}
+              style={{
+                height: "35px",
+                width: "35px",
+                backgroundColor: "transparent",
+                borderRadius: "10px",
+                margin: "auto",
+                padding: "1px",
+              }}
+              alt="Follow us on Instagram"
+            />
+          </a>
+        </address>
       </header>
       <nav style={mainLinksHeaderContainer}>
         {MainLinks.map((link) => (
