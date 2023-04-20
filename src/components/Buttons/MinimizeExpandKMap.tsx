3d711c840b5e770c2f4dd8a7244879a9c1260027
Lokisk1155 <shawn12345d@gmail.com>
2023-04-19 22:32:18 -0400

    darkgrey

--- src/components/Buttons/MinimizeExpandKMap.tsx
+++ src/components/Buttons/MinimizeExpandKMap.tsx
@@ -24,6 +24,7 @@ const MinimizeExpandKMap: React.FC<MinimizeExpandKMapProps> = ({
           viewBox="0 0 24 24"
           width="35px"
           height="35px"
+          fill="darkgrey"
           style={MeKMapStyles}
         >
           <path fill="none" d="M0 0h24v24H0z" />
@@ -38,8 +39,8 @@ const MinimizeExpandKMap: React.FC<MinimizeExpandKMapProps> = ({
           height="35"
           style={MeKMapStyles}
         >
-          <rect x="4" y="11" width="16" height="2" fill="black" />
-          <rect x="11" y="4" width="2" height="16" fill="black" />
+          <rect x="4" y="11" width="16" height="2" fill="darkgrey" />
+          <rect x="11" y="4" width="2" height="16" fill="darkgrey" />
         </svg>
       )}
     </>
