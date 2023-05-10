7cf8f3def4c5be37734aca7ef4d89779bc75f804
Lokisk1155 <shawn12345d@gmail.com>
2023-05-10 00:11:53 -0400

    submit button

--- src/components/Buttons/SubmitSchedule.tsx
+++ src/components/Buttons/SubmitSchedule.tsx
@@ -1,16 +1,15 @@
 import * as React from "react";
 import { CopyInfoButtonStyle } from "../../styles/Buttons";
+import { formDataToObject } from "../../components/Schedule/utils/formDataToObj";
 
 interface SubmitScheduleProps {
   formRef: React.RefObject<HTMLFormElement>;
 }
 
-const SubmitSchedule: React.FC = () => {
-  const formRef = React.useRef<HTMLFormElement>(null);
-
-  const handleCopyInfoButton = (
+const SubmitSchedule: React.FC<SubmitScheduleProps> = ({ formRef }) => {
+  const handleCopyInfoButton = async (
     e: React.MouseEvent<HTMLButtonElement>
-  ): void => {
+  ): Promise<void> => {
     e.preventDefault();
 
     if (!formRef.current) return;
@@ -22,25 +21,29 @@ const SubmitSchedule: React.FC = () => {
       body: JSON.stringify(formDataToObject(formData)),
     };
 
-    const request = await fetch(
-      "https://email-server-shawns-fitness.herokuapp.com/send-email",
-      requestOptions
-    );
+    try {
+      const request = await fetch(
+        "https://email-server-shawns-fitness.herokuapp.com/send-email",
+        requestOptions
+      );
 
-    if (!request) return;
+      if (!request) return;
 
-    const element = document.getElementById("Buisness-Address");
-    const text = element?.innerText;
-    if (text) {
-      navigator.clipboard.writeText(text);
-      const button = document.getElementById("Submit-Inquire-Button");
-      if (button) {
-        button.innerHTML = "&#x2713 Copied to clipboard";
-        button.style.backgroundColor = "lightgreen";
-        button.style.borderColor = "lightgreen";
-        button.style.borderWidth = "4px";
-        button.style.borderStyle = "solid";
+      const element = document.getElementById("Buisness-Address");
+      const text = element?.innerText;
+      if (text) {
+        navigator.clipboard.writeText(text);
+        const button = document.getElementById("Submit-Inquire-Button");
+        if (button) {
+          button.innerHTML = "&#x2713 Copied to clipboard";
+          button.style.backgroundColor = "lightgreen";
+          button.style.borderColor = "lightgreen";
+          button.style.borderWidth = "4px";
+          button.style.borderStyle = "solid";
+        }
       }
+    } catch (error) {
+      console.error("Error:", error);
     }
   };
 
