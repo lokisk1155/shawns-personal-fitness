8f3866938d7d282ab6aeb032ef5180ee248a7189
Lokisk1155 <shawn12345d@gmail.com>
2023-05-09 23:28:14 -0400

    styling + remove uncess

--- src/components/Schedule/DTForm.tsx
+++ src/components/Schedule/DTForm.tsx
@@ -20,37 +20,23 @@ const DTForm: React.FC = () => {
   };
 
   return (
-    <form ref={formRef} onSubmit={handleSubmit}>
-      <h2>Contact Form</h2>
-      <label htmlFor="desiredTimes">Desired times:</label>
-      <input type="text" id="desiredTimes" name="desiredTimes" required />
-      <br />
-      <label htmlFor="specificTrainer">Specific trainer:</label>
-      <input type="text" id="specificTrainer" name="specificTrainer" required />
-      <br />
+    <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", backgroundColor: "#fff", height: "500px", width: "500px"}}>
+    <form ref={formRef} onSubmit={handleSubmit} style={{ display: "flex", flexDirection: "column", justifyContent: "space-evenly", backgroundColor: "#fff", height: "80%", width: "80%", border: "1px solid grey"}}>
       <label htmlFor="firstName">First Name:</label>
       <input type="text" id="firstName" name="firstName" required />
-      <br />
       <label htmlFor="lastName">Last Name:</label>
       <input type="text" id="lastName" name="lastName" required />
-      <br />
       <label htmlFor="email">Email:</label>
       <input type="email" id="email" name="email" required />
-      <br />
       <label htmlFor="phoneNumber">Phone number:</label>
       <input type="tel" id="phoneNumber" name="phoneNumber" required />
-      <br />
       <label htmlFor="address">Address:</label>
       <input type="text" id="address" name="address" required />
-      <br />
       <label htmlFor="bestTimes">Best times to reach out:</label>
       <input type="text" id="bestTimes" name="bestTimes" required />
-      <br />
-      <label htmlFor="socialMedia">Follow us on social media:</label>
-      <input type="checkbox" id="socialMedia" name="socialMedia" />
-      <br />
       <button type="submit">Submit</button>
     </form>
+    </div>
   );
 };
 
